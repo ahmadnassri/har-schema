080628ccfbaba3d4fe0716cc0252df0b1d4ba6f4
Ahmad Nassri <ahmad@ahmadnassri.com>
2016-12-03 20:08:13 -0500

    feat(package): added docs, tests with travis, package details, contributors

---
+++ test/index.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const schemas = require('../lib')
+const Ajv = require('ajv')
+const tap = require('tap')
+
+tap.test('har-schema', (assert) => {
+  const ajv = new Ajv()
+  const keys = Object.keys(schemas)
+
+  assert.plan(keys.length * 2)
+
+  keys.forEach((key) => assert.doesNotThrow(ajv.addSchema(schemas[key])))
+  keys.forEach((key) => assert.type(ajv.compile(schemas[key]), 'function'))
+})
